753a378646b0d8883262d58e46fba52407353845
rafaelbrier <rafael.sales@ntendencia.com.br>
2021-02-06 17:26:04 -0300

    [REVIEW]: DEBOUNCE

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -2,26 +2,29 @@ import React from 'react';
 import useStyles from './styles';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
+import { debounce } from '@material-ui/core';
 
-const SearchInput = ( { onChange } ) => {
+const SearchInput = ({ onChange }) => {
     const styles = useStyles();
 
+    const debouncedOnChange = debounce(onChange, 300);
+
     return (
-        <div className={ styles.searchBox }>
-            <div className={ styles.searchIcon }>
+        <div className={styles.searchBox}>
+            <div className={styles.searchIcon}>
                 <SearchIcon />
             </div>
             <InputBase
                 placeholder='Buscar filme'
-                onChange={ onChange }
-                classes={ {
+                onChange={debouncedOnChange}
+                classes={{
                     root: styles.inputRoot,
                     input: styles.inputInput,
-                } }
-                inputProps={ { 'aria-label': 'search' } }
+                }}
+                inputProps={{ 'aria-label': 'search' }}
             />
         </div>
     );
-}
+};
 
 export default SearchInput;
