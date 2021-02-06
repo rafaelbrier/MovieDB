d9f6380d630a5b321ff9af1d9673172c61d397c1
rafaelbrier <rafael.sales@ntendencia.com.br>
2021-02-06 17:26:39 -0300

    [REVIEW]: QueryParams

--- src/services/apiData.js
+++ src/services/apiData.js
@@ -2,14 +2,18 @@ const key = '2909bb37076c77332f2a706184ab330e';
 const language = 'en-US';
 const timeWindow = 'day';
 
-const apiData = {
-    trending: ( mediaType ) => `/trending/${ mediaType }/${ timeWindow }?api_key=${ key }`,
-    latest: `/movie/latest?api_key=${ key }&language=${ language }`,
-    popular: ( page ) => `/movie/popular?api_key=${ key }&language=${ language }&page=${ page }`,
-    topRated: ( page ) => `/movie/top_rated?api_key=${ key }&language=${ language }&page=${ page }`,
-    movieImage: ( movieId ) => `/movie/{${ movieId }}/images?api_key=${ key }&language=${ language }`,
-    searchMovie: ( movie, page ) => `/search/movie?api_key=${ key }&query=${ movie }&page=${ page }`,
-    config: `/configuration?api_key=${ key }`,
+function buildQueryParams(paramsJson) {
+    return new URLSearchParams(paramsJson).toString();
 }
+
+const apiData = {
+    trending: (mediaType) => `/trending/${mediaType}/${timeWindow}?api_key=${key}`,
+    latest: `/movie/latest?api_key=${key}&language=${language}`,
+    popular: (page) => `/movie/popular?${buildQueryParams({ api_key: key, language, page })}`,
+    topRated: (page) => `/movie/top_rated?api_key=${key}&language=${language}&page=${page}`,
+    movieImage: (movieId) => `/movie/{${movieId}}/images?api_key=${key}&language=${language}`,
+    searchMovie: (movie, page) => `/search/movie?api_key=${key}&query=${movie}&page=${page}`,
+    config: `/configuration?api_key=${key}`,
+};
 
 export default apiData;
